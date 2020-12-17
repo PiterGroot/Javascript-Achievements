bb2563754619ea48f80504ca9996d5405a577afa
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2020-12-17 10:37:47 +0100

    Quiz

---
+++ F1M2JS/les05Quiz/quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Lisser Art Museum",
+        "imageURI": "https://jtravel.nl/images/Bollenstreek/Lisse/Lisse-027.jpg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Wat betekent 'LAM'",
+				"answers": [{
+						"answer": "Lisser Art Museum",
+						"feedback": true
+					},
+					{
+						"answer": "Landgoed Art Museum",
+						"feedback": false
+					},
+					{
+						"answer": "Lisse Art Museum",
+						"feedback": false
+					},
+					{
+						"answer": "Lizard Art Museum",
+						"feedback": false
+					},
+					{
+						"answer": "Lammerschans Art Museum",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wie is geen kunstenaar in het LAM?",
+				"answers": [{
+						"answer": "Anna Eijsbouts",
+						"feedback": false
+					},
+					{
+						"answer": "Gerard Polderen",
+						"feedback": true
+					},
+					{
+						"answer": "Tom Friedman",
+						"feedback": false
+					},
+					{
+						"answer": "Folkert de Jong",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat is de organisatie achter het LAM?",
+				"answers": [{
+						"answer": "Het LAM komt voort uit de meerdere organisaties.",
+						"feedback": false
+					},
+					{
+						"answer": "Het LAM komt voort uit de Art 4 Everyone die buitenlandse werken naar Nederland haalt.",
+						"feedback": false
+					},
+					{
+						"answer": "Het LAM komt voort uit geen organisatie.",
+						"feedback": false
+					},
+					{
+						"answer": "Het LAM komt voort uit de VandenBroek Foundation die kunst toegankelijk wil maken voor een breed publiek. ",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
